a5c922777129f21b37197eb6f94bb99972fb09c7
jenberu <jemberu0970@gmail.com>
2024-12-27 19:38:56 +0300

    Save progress before upgrading

--- components/pharmacyCard.js
+++ components/pharmacyCard.js
@@ -28,10 +28,8 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 10,
     elevation: 3,
-    shadowColor: "#000",
-    shadowOpacity: 0.2,
-    shadowRadius: 5,
-    shadowOffset: { width: 0, height: 2 },
+    boxShadow: '0px 2px 3.84px rgba(0, 0, 0, 0.25)',
+
   },
   image: {
     width: 80,
